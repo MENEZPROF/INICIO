6308cbb6d098322d83e41bc57ac89be6c68a6cbc
Victor Jimenez <vi2tor.nieto@gmail.com>
2025-04-11 20:38:37 -0500

    Version 2 - Rama 2

---
+++ document.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded',function(){
+
+let caja=document.getElementById("contenedor")
+console.log(caja)
+
+})
---
+++ funcionesreturn.js
@@ -0,0 +1,12 @@
+function suma(numero1,numero2){
+    let resultado=numero1+numero2
+    return resultado
+
+
+}
+
+suma(200,800)
+
+
+
+
